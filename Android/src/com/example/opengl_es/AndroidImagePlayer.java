5f4e4b80924923d19580de92424fb61483ee627b
lhn <sbwxfce@163.com>
2017-03-20 17:21:52 +0800

    初始导入

---
+++ Android/src/com/example/opengl_es/AndroidImagePlayer.java
@@ -0,0 +1,96 @@
+package com.example.opengl_es;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import android.app.Activity;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.opengl.GLES20;
+import android.opengl.GLUtils;
+import android.os.Environment;
+import android.util.Log;
+
+public class AndroidImagePlayer 
+{
+	private static final String TAG = "ShowImage";
+	private int UnityTextureID = -1;
+	private Activity activity;
+	private Bitmap bitmap;
+	public AndroidImagePlayer(Activity ac)
+	{
+		this.activity = ac;
+	}
+	public void SetImagePath(String path)
+	{
+		SetImagePath(path,false);
+	}
+	public void SetImagePath(String path,boolean isSDFile)
+	{
+		//bitmap = BitmapFactory.decodeFile(Environment.getExternalStorageDirectory().getPath()+"/Images/test.jpg");//BitmapFactory.decodeResource(activity.getResources(), R.drawable.bg);
+		if(bitmap!=null)
+		{
+			bitmap.recycle();
+			bitmap = null;
+		}
+		if(isSDFile)
+		{
+			bitmap = BitmapFactory.decodeFile(Environment.getExternalStorageDirectory().getPath()+"/"+path);
+		}else
+		{
+			bitmap = BitmapFactory.decodeFile(path);
+//			AssetManager assetManager = activity.getAssets();
+//			try 
+//			{
+//				InputStream stream = assetManager.open(path);
+//				bitmap = BitmapFactory.decodeStream(stream);
+//			} catch (IOException e) 
+//			{
+//				e.printStackTrace();
+//			}
+		}
+		if(bitmap==null)
+		{
+			Log.d(TAG,"无法获取到Bitmap");
+		}
+		rendererImage();
+	}
+	
+	
+	public int SetTexID(int id)
+	{
+		if(id==-1)
+		{
+			int[] textureHandle = new int[1];
+			// 生成纹理id，可以一次生成多个，后续操作纹理全靠这个id 
+			GLES20.glGenTextures(1, textureHandle, 0);
+			id = textureHandle[0];
+		}
+		UnityTextureID = id;
+		Log.d(TAG,"--->纹理ID："+id);
+		rendererImage();
+		return id;
+	}
+	
+	private void rendererImage()
+	{
+		if(bitmap==null||UnityTextureID==-1)return;
+		Log.d(TAG,"--->绘制图片到纹理中	ID："+UnityTextureID);
+		// 操作纹理，传入纹理id作为参数，每次bind之后，后续操作的纹理都是该纹理 
+		GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, UnityTextureID);
+		//指定纹理格式。这里包括纹理横向和纵向的重复方式 
+		//GL_TEXTURE_WRAP_S 
+		//GL_TEXTURE_WRAP_T 
+		//纹理在放大和缩小（同样纹理离远和离近）时的处理，这种设置主要是为了避免同一个纹理反复使用时，远处的纹理反而比近处的清晰 
+		//GL_TEXTURE_MAG_FILTER 
+		//GL_TEXTURE_MIN_FILTER
+		GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
+		GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+		GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE);
+		GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE);
+		//给纹理传入图像数据
+		GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0);
+	}
+	
+}
---
+++ Unity/Assets/Scripts/AndroidImagePlayer.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AndroidImagePlayer : MonoBehaviour 
+{
+	private AndroidJavaObject mainContext;
+	private AndroidJavaObject player;
+	private Texture2D texture;
+	private bool useMobileID = false;
+	private Boolean isNeedSetTexture = false;
+	private Boolean isAndroid = false;
+    // 默认读取Android SD卡上的目录，比如设置为 Images/bg.jpg  表示读取的路径为  /storage/emulated/0/Images/bg.jpg
+    public string imagePath;
+
+	void Start () 
+	{
+		isAndroid = Application.platform == RuntimePlatform.Android;
+		if (!isAndroid)return;
+		AndroidJavaClass ajc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+		mainContext = ajc.GetStatic<AndroidJavaObject>("currentActivity");
+        player = new AndroidJavaObject ("com.example.opengl_es.AndroidImagePlayer", mainContext);
+    }
+	void Update () 
+	{
+		if (!isAndroid)return;
+        if (imagePath == null|| imagePath=="") return;
+		if (texture == null) 
+		{
+            StartCoroutine(CopyStreamingAssetAndLoad(imagePath));
+            //SetImagePath();
+            CreateTexture ();
+		} else 
+		{
+			if (isNeedSetTexture) {
+				UpdateRendererTexture ();
+			}
+			GL.InvalidateState ();
+		}
+
+        //StartCoroutine();
+	}
+	private void UpdateRendererTexture()
+	{
+		if (texture == null)return;
+		isNeedSetTexture = false;
+		MeshRenderer renderer = GetComponent<MeshRenderer> ();
+		if (renderer != null) 
+		{
+			if (renderer.material.mainTexture != texture)  
+				renderer.material.mainTexture = texture;
+		}
+	}
+    private void SetImagePath()
+    {
+        //string filePath = System.IO.Path.Combine(Application.streamingAssetsPath, imagePath);
+        //string str = Application.streamingAssetsPath +"/"+ imagePath;
+        Debug.Log("-->"+imagePath);
+        player.Call("SetImagePath", imagePath);
+    }
+
+
+    private void CreateTexture()
+	{
+		if (useMobileID == false)
+		{
+			texture = new Texture2D (1024, 768, TextureFormat.ARGB32, false);
+            player.Call<int> ("SetTexID",(int)texture.GetNativeTexturePtr());
+		} else 
+		{
+			int tex = player.Call<int>("SetTexID",-1);
+			texture = Texture2D.CreateExternalTexture (1024, 768, TextureFormat.ARGB32, false, false, new IntPtr(tex));
+			//texture.UpdateExternalTexture ();
+		}
+		isNeedSetTexture = true;
+	}
+
+
+    IEnumerator CopyStreamingAssetAndLoad(string strURL)
+    {
+        strURL = strURL.Trim();
+        string write_path = Application.persistentDataPath + "/" + strURL;
+        if (System.IO.File.Exists(write_path) == false)
+        {
+            Debug.Log("CopyStreamingAssetAndLoad : " + strURL);
+            WWW www = new WWW(Application.streamingAssetsPath + "/" + strURL);
+            yield return www;
+            if (string.IsNullOrEmpty(www.error))
+            {
+                Debug.Log(write_path);
+                System.IO.File.WriteAllBytes(write_path, www.bytes);
+                //imagePath = "file://" + write_path;
+                imagePath = write_path;
+                SetImagePath();
+            }
+            else
+            {
+                Debug.Log(www.error);
+            }
+            www.Dispose();
+            www = null;
+        }
+        else
+        {
+            imagePath = write_path;
+            //imagePath = "file://" + write_path;
+            SetImagePath();
+        }
+    }
+}
